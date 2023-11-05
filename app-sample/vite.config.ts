099da983bacb11c408c1b27ab6643aab851e997d
RandyZhang <zhangshi3421@gmail.com>
2023-11-05 16:43:07 +0800

    应用层面添加两个插件

--- app-sample/vite.config.ts
+++ app-sample/vite.config.ts
@@ -1,7 +1,17 @@
 import { createViteConfig } from '@rchitect-cli/vite'
-import { defineConfig, UserConfig } from 'vite'
+import { defineConfig, PluginOption, UserConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig(async ({ command, mode }): Promise<UserConfig> => {
   const config: UserConfig = await createViteConfig(command, mode, process.cwd(), { preset: 'antd' })
+  const autoImport = AutoImport({
+    resolvers: [ElementPlusResolver()],
+  }) as PluginOption
+  const components = Components({
+    resolvers: [ElementPlusResolver()],
+  }) as PluginOption
+  config.plugins?.push(autoImport, components)
   return config;
 })
