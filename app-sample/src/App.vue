70d90385e1f426acb2dffb7af08c0d4dbc613a94
RandyZhang <zhangshi3421@gmail.com>
2023-11-05 16:43:07 +0800

    应用层面添加两个插件

--- app-sample/src/App.vue
+++ app-sample/src/App.vue
@@ -1,17 +1,62 @@
 <script setup lang="ts">
+import type { Directive, VNode, VNodeArrayChildren } from 'vue-demi'
 import HelloWorld from './components/HelloWorld.vue'
+import capitalize from 'lodash-es/capitalize';
+import has from 'lodash-es/has';
+const isVNode = (vnode: any): vnode is VNode => {
+  return vnode && vnode.__v_isVNode === true
+}
+const vThrottleOn: Directive = {
+  mounted: (el, binding, vnode, prevVnode) => {
+    console.log('mounted', el, binding, vnode, prevVnode)
+    const eventName = binding.arg
+    const targetVnode = (vnode.children as VNodeArrayChildren)[0]
+    if (eventName && isVNode(targetVnode)) {
+      const props = targetVnode.props
+      if (props && has(props, `on${capitalize(eventName)}`)) {
+        const originalFunc = props[`on${capitalize(eventName)}`] as (...args: any[]) => Promise<any>
+        const status = {
+          isThrottled: false,
+          originalFunc,
+        }
+        props['____RANDY_SET_STATUS____'] = status
+        props[`on${capitalize(eventName)}`] = async (...args: any[]) => {
+          if (status.isThrottled) {
+            return
+          }
+          status.isThrottled = true
+         await originalFunc(...args)
+         status.isThrottled = false
+        }
+      }
+    }
+  },
+  beforeUnmount(el, binding, vnode, prevVnode) {
+    console.log('beforeUnmount', el, binding, vnode, prevVnode)
+    if (vnode.props && vnode.props['____RANDY_SET_STATUS____']) {
+      const status = vnode.props['____RANDY_SET_STATUS____']
+      delete vnode.props['____RANDY_SET_STATUS____']
+      vnode.props[`on${capitalize(binding.arg)}`] = status.originalFunc
+    }
+  }
+}
+
+const asyncCallback = async (...args: any[]) => {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('真正的函数执行了～～')
+      resolve(true)
+    }, 2000)
+  })
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <!-- <HelloWorld v-throttle-on:custom="asyncCallback" msg="Vite + Vue" /> -->
+  <div v-throttle-on:custom>
+    <HelloWorld @custom="asyncCallback" msg="Vite + Vue directive" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
@@ -21,9 +66,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
--- app-sample/src/components/HelloWorld.vue
+++ app-sample/src/components/HelloWorld.vue
@@ -1,34 +1,43 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { Ref, ref } from 'vue'
 
 defineProps<{ msg: string }>()
 
 const count = ref(0)
+const emit = defineEmits<{
+  custom: [id: number, data:string]
+}>()
+const onClick = () => {
+  count.value++
+  emit('custom', count.value, `count is ${count.value}`)
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <div>
+    <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <div class="card">
+      <button type="button" @click="onClick">count is {{ count }}</button>
+      <p>
+        Edit
+        <code>components/HelloWorld.vue</code> to test HMR
+      </p>
+    </div>
+
     <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      Check out
+      <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+        >create-vue</a
+      >, the official Vue + Vite starter
     </p>
+    <p>
+      Install
+      <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
+      in your IDE for a better DX
+    </p>
+    <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
