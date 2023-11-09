04865fc93de0eae6e8a0dbd1dc0526e3c284d616
RandyZhang <zhangshi3421@gmail.com>
2023-11-09 21:46:02 +0800

    增加模块生成命令

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 基于MonoRepo形式组织的Vue的企业级模块化工程
 
 编译思想和组件设计方案借鉴了很多[Vben](https://github.com/vbenjs/vben3)的思想
+
+## 基础命令
+
+### 模块生成
+
+```bash
+yarn run gen:app <module-name>
+yarn run gen:rock <module-name>
+```
--- design-cli/scripts/src/gen-module.ts
+++ design-cli/scripts/src/gen-module.ts
@@ -1,27 +1,62 @@
 import { execNpmCmd } from "./base";
+const prompts = require('prompts')
 
 const args = process.argv.slice(2);
-console.log('process.argv：\n', process.argv)
-console.log('参数：\n', args)
-const [moduleName, destination = null, template = '@rchitect-cli/template'] = args;
 
-if (!moduleName) {
-  console.log('请指定模块名称');
-  process.exit(1);
-}
-if (!template) {
-  console.log('请指定模板名称');
-  process.exit(1);
-}
-// turbo gen workspace --name @rchitect-app/test --type package --copy @rchitect-cli/template
-const cmd = [
-  'turbo', 'gen', 'workspace', '--root', '../../',
-  '--name', `@rchitect-app/${moduleName}`,
-  '--type', 'package',
-  '--copy', template
-];
-if (destination) {
-  cmd.push('--destination', destination)
-}
-console.log('执行命令：\n', cmd.join(' '))
-execNpmCmd(cmd);
+export type ModuleLevel = 'app' | 'cli' | 'def' | 'rock' | 'demo'
+export type ModuleType = 'package' | 'app'
+
+const [
+  moduleLevel,
+  moduleName,
+  moduleType = 'package',
+  destination = `design-${moduleLevel}/${moduleLevel}-${moduleName}`,
+  template = '@rchitect-cli/template',
+  root = '../../'
+] = args;
+
+/**
+ * 生成模块
+ * 
+ * @param moduleName 模块名称
+ * @param moduleLevel 模块级别
+ * @param moduleType 模块类型
+ * @param destination 目标目录
+ * @param root 模版模块名称
+ * @param template 模版模块名称
+ */
+export const generateModule = (
+  moduleName: string,
+  moduleLevel: ModuleLevel,
+  moduleType: ModuleType,
+  root: string | null = null,
+  destination: string | null = null,
+  template: string = '@rchitect-cli/template'
+) => {
+  if (!moduleName) {
+    console.warn('请指定模块名称');
+    process.exit(1);
+  }
+  if (!template) {
+    console.warn('请指定模板名称');
+    process.exit(1);
+  }
+  if (!root) {
+    console.warn('请指定模板根目录');
+    process.exit(1);
+  }
+
+  const cmd = [
+    'turbo', 'gen', 'workspace',
+    '--name', `@rchitect-${moduleLevel}/${moduleName}`,
+    '--type', moduleType,
+    '--copy', template
+  ];
+  if (destination) {
+    cmd.push('--destination', '../../' + destination)
+  }
+  console.log('执行命令：\n', cmd.join(' '))
+  execNpmCmd(cmd);
+};
+
+generateModule(moduleName, moduleLevel as ModuleLevel, moduleType as ModuleType, root, destination, template)
