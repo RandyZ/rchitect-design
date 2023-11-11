9f504bf75ee4ca12ba8b307d3293bfa614adaebb
RandyZhang <zhangshi3421@gmail.com>
2023-11-12 00:39:02 +0800

    去掉demo中的UI库，准备放入组件

--- app-sample/vite.config.ts
+++ app-sample/vite.config.ts
@@ -1,17 +1,17 @@
 import { createViteConfig } from '@rchitect-cli/vite'
 import { defineConfig, PluginOption, UserConfig } from 'vite'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// import AutoImport from 'unplugin-auto-import/vite'
+// import Components from 'unplugin-vue-components/vite'
+// import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig(async ({ command, mode }): Promise<UserConfig> => {
   const config: UserConfig = await createViteConfig(command, mode, process.cwd(), { preset: 'antd' })
-  const autoImport = AutoImport({
-    resolvers: [ElementPlusResolver()],
-  }) as PluginOption
-  const components = Components({
-    resolvers: [ElementPlusResolver()],
-  }) as PluginOption
-  config.plugins?.push(autoImport, components)
+  // const autoImport = AutoImport({
+  //   resolvers: [ElementPlusResolver()],
+  // }) as PluginOption
+  // const components = Components({
+  //   resolvers: [ElementPlusResolver()],
+  // }) as PluginOption
+  // config.plugins?.push(autoImport, components)
   return config;
 })
