1bbecacfcbcf154021ed9323b621808ee04d605d
RandyZhang <zhangshi3421@gmail.com>
2023-11-11 13:27:17 +0800

    修改工程模版

--- app-sample/src/main.ts
+++ app-sample/src/main.ts
@@ -2,5 +2,25 @@ import '@rchitect-design/styles';
 import 'virtual:svg-icons-register';
 import { createApp } from 'vue'
 import App from './App.vue'
+import { APP_CONTEXT, AppContext } from '@rchitect-rock/base-package';
+import {
+  IocPlugin,
+  IocContainerOptions,
+  AsyncIocModule,
+  THROWN_HANDLER,
+  diKT,
+} from '@rchitect-rock/ioc';
+import { Lib as routeLib } from '@rchitect-rock/router';
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+(async () => {
+  const app = createApp(App);
+  const appContext = new AppContext();
+  // IOC插件配置
+  const iocOptions = {
+    defaultScope: 'Singleton',
+    autoBindInjectable: true,
+    skipBaseClassChecks: true,
+  } as IocContainerOptions;
+  
+})()
