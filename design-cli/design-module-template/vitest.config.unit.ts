6e903c4f7f6070e1b451f8e800e1c47614cd37f8
RandyZhang <zhangshi3421@gmail.com>
2024-01-15 12:18:34 +0800

    修改命令

--- design-cli/design-module-template/vitest.config.unit.ts
+++ design-cli/design-module-template/vitest.config.unit.ts
@@ -1,8 +1,10 @@
 import { defineProject } from 'vitest/config'
 import Vue from '@vitejs/plugin-vue'
+import { MonoRepoResolverPlugin } from "@rchitect-cli/vite";
 export default defineProject({
   plugins: [
     Vue(),
+    MonoRepoResolverPlugin()
   ],
   test: {
     globals: true,
